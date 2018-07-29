cdb137b854127f8e0b965961740a06de99117f08
Nephi Wright <nephiw@gmail.com>
2018-07-29 13:29:30 -0600

    Make site informational about what happened last year.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AgmCoreModule } from '@agm/core';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
 
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-import { LocationsComponent } from './locations/locations.component';
-import { SitesService } from './sites.service';
-import { ToStringPipe } from './to-string.pipe';
-import { DetailComponent } from './locations/detail.component';
-import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    CommonModule,
-    FormsModule,
-    AgmCoreModule.forRoot({ apiKey: environment.mapsKey }),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    CommonModule
   ],
   declarations: [
-    AppComponent,
-    LocationsComponent,
-    ToStringPipe,
-    DetailComponent,
-    SignupComponent
+    AppComponent
   ],
-  providers: [SitesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
